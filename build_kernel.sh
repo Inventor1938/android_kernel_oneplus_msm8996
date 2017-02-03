f2b09513613e7167f9a42c16bc2db9e8f243ddbb
joshuous <joshuous@gmail.com>
2017-02-03 06:15:48 +0000

    build_kernel: Update RELEASE names

--- build_kernel.sh
+++ build_kernel.sh
@@ -23,7 +23,7 @@ DTBIMAGE="dtb"
 # Kernel Details
 KERNEL=Zenith
 DEVICE="OP3"
-VERSION="001"
+VERSION="002"
 RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}"
 
 # Vars
@@ -107,26 +107,25 @@ select choice in default LINARO-aarch64-linux-android-4.9 LINARO-aarch64-linux-a
 do
 case "$choice" in
   "default")
-    RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}"
     break;;
   "LINARO-aarch64-linux-android-4.9")
     export CROSS_COMPILE="$TOOLCHAIN_DIR/aarch64-linux-android-linaro-4.9-kernel/bin/aarch64-linux-android-"
-    RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}-LINARO-4.9"
+    RELEASE="${RELEASE}-LINARO-4.9"
     break;;
   "LINARO-aarch64-linux-android-5.x")
     export CROSS_COMPILE="$TOOLCHAIN_DIR/aarch64-linux-android-linaro-5.x-kernel/bin/aarch64-linux-android-"
-    RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}-LINARO-5.x"
+    RELEASE="${RELEASE}-LINARO-5.x"
     break;;
   "LINARO-aarch64-linux-android-6.x")
     export CROSS_COMPILE="$TOOLCHAIN_DIR/aarch64-linux-android-linaro-6.x-kernel/bin/aarch64-linux-android-"
-    RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}-LINARO-6.x"
+    RELEASE="${RELEASE}-LINARO-6.x"
     break;;
   "UBER-aarch64-linux-android-4.9")
-    RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}-UBER-4.9"
+    RELEASE="${RELEASE}-UBER-4.9"
     export CROSS_COMPILE="$TOOLCHAIN_DIR/aarch64-linux-android-uber-4.9-kernel/bin/aarch64-linux-android-"
     break;;
   "UBER-aarch64-linux-android-5.3")
-    RELEASE="${KERNEL}.${DEVICE}.EAS.${VERSION}-UBER-5.3"
+    RELEASE="${RELEASE}-UBER-5.3"
     export CROSS_COMPILE="$TOOLCHAIN_DIR/aarch64-linux-android-uber-5.3-kernel/bin/aarch64-linux-android-"
     break;;
 esac
